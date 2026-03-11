ab1144085f732ea6aefbcf71898dd9e6a85b2783
Caio-T4vares <caio.hosana@hotmail.com>
2026-03-11 14:13:08 -0300

    Add operate function
    Add function to decide what kind of operation will apply on the 2
    operands

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function multiply(firstOperand, secondOperand) {
 function divide(firstOperand, secondOperand) {
   return firstOperand / secondOperand;
 }
+
+function operate(firstOperand, secondOperand, operator) {
+  switch (operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+    case "-":
+      return subtract(firstOperand, secondOperand);
+    case "*":
+      return multiply(firstOperand, secondOperand);
+    case "/":
+      return divide(firstOperand, secondOperand);
+    default:
+      alert("Invalid operator");
+      return NaN;
+  }
+}
